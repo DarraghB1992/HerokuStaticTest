44b70066b316730ac469f2a9d27c6b02f26bb999
Darragh <darragh1992@gmail.com>
2016-04-21 11:01:10 +0100

    static fix2

--- settings/base.py
+++ settings/base.py
@@ -96,10 +96,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 )
 
 TINYMCE_JS_ROOT = os.path.join(BASE_DIR, "static", "js",
